7fe4a1db8a62302f8ff52e165c7de0b2a5dbbb8a
Bighnesh Kumar Pati <bighneshpati@gmail.com>
2022-05-26 18:01:09 +0530

    added type and description

--- variables.tf
+++ variables.tf
@@ -1,9 +1,15 @@
 variable "prefix" {
   default = "Mr"
+  type = string
+  description = "This is the variable for the prefix of the animal"
 }
 variable "separator" {
   default = " "
+  type = string
+  description = "This act's as the separator between words"
 }
 variable "length" {
   default = 3
+  type = number
+  description = "This is the no. of words produced"
 }
