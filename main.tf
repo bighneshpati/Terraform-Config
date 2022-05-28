fc4eb2d4598d50e95142303f3514b0ecd148d996
Bighnesh Kumar Pati <bighneshpati@gmail.com>
2022-05-28 20:32:50 +0530

    add the pet name

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
-resource "random_pet" pet{
+resource "local_file" pet_name {
+  filename = "/Users/bighneshmacbook/terraform_test/petname.txt"
+  content = "Hello my pet name is ${random_pet.pet.id}"
+}
+resource "random_pet" pet {
 	prefix = var.prefix
 	separator = var.separator
 	length = var.length
